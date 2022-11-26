1ef856917a85631dbdd174d093e9fd348138e15f
Edison Ibáñez <arkhan@riseup.net>
2022-11-26 12:19:47 -0500

    [ADD] res.partner.child

--- res_partner_check_vat/models/partner.py
+++ res_partner_check_vat/models/partner.py
@@ -1,5 +1,20 @@
-from odoo import models
+from odoo import models, fields, _, api
 
-class ResPartnerExt(models.Model):
-    _inherit = 'res.partner'
-    
+
+class ResPartnerChild(models.Model):
+    """Partner Children"""
+
+    _name = "res.partner.child"
+    _description = __doc__
+
+    partner_id = fields.Many2one('res.partner', string=_("Parent"))
+    name = fields.Char(_("Name"), required=True)
+    birthday = fields.Date(_('Birthday'), required=True)
+#    age = fields.Float(_('Age'), compute="_get_age")
+
+#    @api.multi
+#    def _get_age(self):
+#        for row in self:
+#            if row.birthday:
+#                row.age = fields.Date.today().year - row.birthday.year
+#        return True
