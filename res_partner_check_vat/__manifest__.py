e3df66c52bc715c1d8628cd1b866c13eb28d15d3
Edison Ibáñez <arkhan@riseup.net>
2022-11-26 11:24:06 -0500

    first commit

---
+++ res_partner_check_vat/__manifest__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+{
+    "name": "Partner - Check VAT (EC)",
+    "version": "1.0.0",
+    "summary": "Check Valid VAT (EC)",
+    "description": "",
+    "category": "",
+    "website": "",
+    "images": [],
+    "depends": ["base",],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ res_partner_check_vat/models/partner.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class ResPartnerExt(models.Model):
+    _inherit = 'res.partner'
+    
