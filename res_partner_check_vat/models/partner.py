75ae46f055dc21745e10c4951dfc58997aa6fbda
Edison Ibáñez <arkhan@riseup.net>
2022-12-03 09:43:50 -0500

    [FIX] validación inicial del DNI

--- res_partner_check_vat/models/partner.py
+++ res_partner_check_vat/models/partner.py
@@ -1,4 +1,6 @@
-from odoo import models, fields, _, api
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from stdnum.ec import ci
 
 
 class ResPartnerChild(models.Model):
@@ -7,14 +9,51 @@ class ResPartnerChild(models.Model):
     _name = "res.partner.child"
     _description = __doc__
 
-    partner_id = fields.Many2one('res.partner', string=_("Parent"))
+    partner_id = fields.Many2one("res.partner", string=_("Parent"))
     name = fields.Char(_("Name"), required=True)
-    birthday = fields.Date(_('Birthday'), required=True)
-#    age = fields.Float(_('Age'), compute="_get_age")
+    lastname = fields.Char(_("Lastname"))
+    birthday = fields.Date(_("Birthday"))
 
-#    @api.multi
-#    def _get_age(self):
-#        for row in self:
-#            if row.birthday:
-#                row.age = fields.Date.today().year - row.birthday.year
-#        return True
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    type_vat = fields.Selection(
+        [("ruc", _("RUC")), ("dni", _("Cédula")), ("pasaporte", _("Pasaporte"))],
+        string=_("Type VAT"),
+        default="dni",
+    )
+    relative_ids = fields.One2many(
+        "res.partner.child", "partner_id", string=_("Partner relatives")
+    )
+
+    # Validación en linea mediante @api.onchange
+    # @api.onchange("type_vat", "vat")
+    # def _onchage_vat(self):
+    #     res = {"warning": {}}
+    #     title = ""
+    #     msg = ""
+    #     if self.vat and self.type_vat == "dni":
+    #         if len(self.vat) != 10:
+    #             title = "Error de longitud!"
+    #             msg = "¡El numero de cedula debe tener 10 caracteres!"
+    #     if title and msg:
+    #         res["warning"].update(
+    #             {
+    #                 "title": title,
+    #                 "message": msg,
+    #                 "type": "notification",
+    #             },
+    #         )
+    #     return res
+
+    @api.constrains("company_id", "type_vat", "vat")
+    def _check_vat_unique(self):
+        for row in self:
+            if row.type_vat == "dni":
+                if len(row.vat) != 10:
+                    raise ValidationError(
+                        _("¡El numero de cedula debe tener 10 caracteres!")
+                    )
+                if not ci.is_valid(row.vat):
+                    raise ValidationError(_("¡El numero de cedula no es valido!"))
